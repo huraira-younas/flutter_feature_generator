4ae8c6017a0b619883b359bbf108438ebb92c0cc
Huraira Younas <raohuraira331.rb@gmail.com>
2025-10-24 14:38:25 +0500

    Fixed Bugs

--- src/templates/repository/feature_repository.dart
+++ src/templates/repository/feature_repository.dart
@@ -4,7 +4,5 @@ import '{{feature_name}}_client.dart';
 class {{feature_class}}Repository implements {{feature_class}}Interface {
   final _client = {{feature_class}}Client();
 
-  {{feature_class}}Repository({required this.client});
-
   // TODO: Implement repository methods
 }
